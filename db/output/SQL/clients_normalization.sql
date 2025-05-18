ca727670ead0be32f2af73427c5389550eb17515
“Nick <nikitaprime83@gmail.com>
2025-05-18 15:02:16 +0300

    Add: full product table normalization

---
+++ db/output/SQL/clients_normalization.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:27c3ad7416dde6472a61766f586f4e69a1aa984035ec813192597d2500ba5010
+size 470
---
+++ db/output/SQL/products_normalization.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:59aae45c8e38db011053d2ddd9b91eaf66d3cf577de5d060c0ff640e18039a19
+size 1302
